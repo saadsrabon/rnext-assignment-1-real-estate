1bf27c5fdf9a1faa44b356ca03520357d36ce255
Saad Srabon <saadsrabon2@gmail.com>
2024-11-25 10:44:54 +0600

    resuable component for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
-
+const Buttons= ({title})=> <a
+href="signin.html"
+className="flex-1 loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70"
+>
+{title}
+</a>
 export default function Header() {
+  const navItems = [
+    { name: "Home", link: "#home" },
+    { name: "About", link: "#about" },
+    { name: "Pricing", link: "#pricing" },
+    { name: "Blog" },
+  ];
+
+  
+
   return (
     // <!-- ====== Navbar Section Start -->
 
@@ -7,54 +21,38 @@ export default function Header() {
       <div className="container">
         <div className="relative -mx-4 flex items-center justify-between">
           <div className="flex w-full items-center justify-between px-4 py-3">
-            <a href="index.html" className="navbar-logo block w-60 max-w-full px-4">
-              <img src="assets/images/logo/logo.svg" alt="logo" className="header-logo h-12" />
+            <a
+              href="index.html"
+              className="navbar-logo block w-60 max-w-full px-4"
+            >
+              <img
+                src="assets/images/logo/logo.svg"
+                alt="logo"
+                className="header-logo h-12"
+              />
             </a>
-  
-  
+
             <ul className="hidden lg:flex ">
-              <li className="group relative">
-                <a href="#home"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70">
-                  Home
-                </a>
-              </li>
-              <li className="group relative">
-                <a href="#about"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10">
-                  About
-                </a>
-              </li>
-              <li className="group relative">
-                <a href="#pricing"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10">
-                  Pricing
-                </a>
-              </li>
-  
-              <li className="group relative">
-                <a href="blog-grids.html"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10">
-                  Blog
-                </a>
-              </li>
-  
+              {navItems.map((item, index) => (
+                <li className="group relative" key={index}>
+                  <a
+                    href={item?.link}
+                    className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70"
+                  >
+                    {item?.name}
+                  </a>
+                </li>
+              ))}
             </ul>
-  
-            <div className="sm:flex">
-              <a href="signin.html"
-                className="flex-1 loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70">
-                Sign In
-              </a>
-              <a href="signup.html"
-                className="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
-                Sign Up
-              </a>
+
+            <div className="sm:flex ">
+            <Buttons title="SignUp"/>
+            <Buttons title="Sign in"/>
             </div>
           </div>
         </div>
       </div>
     </div>
     // <!-- ====== Navbar Section End -->
-  )
+  );
 }
