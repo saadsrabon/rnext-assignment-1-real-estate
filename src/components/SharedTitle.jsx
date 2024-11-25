c928c03345a0541f841d422b7e384f02bade7661
Saad Srabon <saadsrabon2@gmail.com>
2024-11-25 11:07:07 +0600

    Why choose us section added

---
+++ src/components/SharedTitle.jsx
@@ -0,0 +1,16 @@
+
+export default function SharedTitle({subtitle,title,description}) {
+  return (
+    <div className="mx-auto mb-12 max-w-[485px] text-center lg:mb-[70px]">
+            <span className="mb-2 block text-lg font-semibold text-primary">
+              {subtitle}
+            </span>
+            <h2 className="mb-3 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]">
+              {title}
+            </h2>
+            <p className="text-base text-body-color dark:text-dark-6">
+              {description}
+            </p>
+          </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Available from "../components/Available";
 import Contact from "../components/Contact";
 import Cta from "../components/Cta";
 import Faq from "../components/Faq";
-import Feature from "../components/Feature";
+import Feature from "../components/Features";
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
