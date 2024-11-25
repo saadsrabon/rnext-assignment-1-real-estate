c8b99e55bf8603aca613ead27aab9dfa6fc4757c
Saad Srabon <saadsrabon2@gmail.com>
2024-11-25 10:53:39 +0600

    Button fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,14 @@
+const Button = ({ text }) => {
+    return (
+        <li>
+        <a href="https://links.tailgrids.com/play-download"
+        className={`${text==="BOOK"?"inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color":"flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark" }`}
+       >
+        {text}
+      </a>
+      </li>
+    );
+};
 export default function Hero() {
   return (
     // <!-- ====== Hero Section Start -->
@@ -14,18 +25,8 @@ export default function Hero() {
               We have 9000 more review and our customers trust on out property and quality products.
             </p>
             <ul className="mb-10 flex flex-wrap items-center justify-center gap-5">
-              <li>
-                <a href="https://links.tailgrids.com/play-download"
-                  className="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color">
-                  Book Now
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/tailgrids/play-tailwind" target="_blank"
-                  className="flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark">
-                  Contact Us
-                </a>
-              </li>
+             <Button text="BOOK"/>
+              <Button text="Contact Us"/>
             </ul>
 
           </div>
